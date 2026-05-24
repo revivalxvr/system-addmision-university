381acbf90a314bf471279231b67f5b9541235b8f
revivalxvr <revivaliskandar@gmail.com>
2026-05-24 12:05:34 +0800

    get year by id

--- src/controllers/Year.controller.js
+++ src/controllers/Year.controller.js
@@ -8,3 +8,42 @@ import { successResponse, errorResponse } from "../utils/response.js";
     // createYear,
     // updateYear,
     // deleteYear,
+
+export const getAllYears = async (req, res) => {
+    try {
+        const tokenCredential = req.user;
+        if (tokenCredential.role !== "admin") {
+            return res.status(401).json({
+                 success: false,
+                 message: "Unauthorized" });
+        }
+        const years = await prisma.academyYear.findMany();
+        return successResponse(res, "berhasil mendapatkan data tahun akademik", years);
+    } catch (error) {
+        return errorResponse(res, "gagal mendapatkan data tahun akademik", null, 500);
+    }
+}
+
+export const getYearById = async (req, res) => {
+    try {
+        const tokenCredential = req.user;
+        if (tokenCredential.role !== "admin") {
+            return res.status(401).json({
+                 success: false,
+                 message: "Unauthorized" });
+        }
+        const {id} = req.params;
+        const year = await prisma.academyYear.findUnique({
+            where :{
+                id
+            }
+        })
+        if(!year || year.length ===0) {
+            return errorResponse(res, "tahun akademik tidak ditemukan", null, 404);
+        }
+        return successResponse(res, "berhasil mendapatkan data tahun akademik", year);
+        
+    } catch (error) {
+         return errorResponse(res, "gagal mendapatkan data tahun akademik", null, 500);
+    }
+}
