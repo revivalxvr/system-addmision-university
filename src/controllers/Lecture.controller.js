d6bd27e0fa5f514635f4802fa098256e98f815d4
revivalxvr <revivaliskandar@gmail.com>
2026-05-25 14:51:21 +0800

    buat function 'create lucture'

--- src/controllers/Lecture.controller.js
+++ src/controllers/Lecture.controller.js
@@ -68,3 +68,35 @@ export const getLectureById = async (req, res) => {
     return errorResponse(res, "terjadi kesalahan", null, 500);
   }
 };
+
+export const createLecture = async (req, res) => {
+  try {
+    //validate the role must be admin to access this route
+    const tokenCredential = req.user;
+    if (tokenCredential.role !== "admin") {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const { name, email, lectureNumber, position, majorId } = req.body;
+    if (!name || !email || !lectureNumber || !position || !majorId) {
+      return errorResponse(res, "data harus diisi", null, 400);
+    }
+    const lecture = await prisma.lecture.create({
+      data: {
+        name,
+        email,
+        lectureNumber: Number(lectureNumber),
+        position,
+        majorId,
+      },
+    });
+    if (!lecture) {
+      return errorResponse(res, "gagal membuat data", null, 500);
+    }
+    return successResponse(res, "berhasil membuat data", lecture);
+  } catch (error) {
+    return errorResponse(res, "terjadi kesalahan", null, 500);
+  }
+};
