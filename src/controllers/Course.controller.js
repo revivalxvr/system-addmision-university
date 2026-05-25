2be2c3084207c279db0a5b3bfe0fbd217ff34e08
revivalxvr <revivaliskandar@gmail.com>
2026-05-25 19:19:10 +0800

    buat function controller "get course by id'

--- src/controllers/Course.controller.js
+++ src/controllers/Course.controller.js
@@ -31,6 +31,44 @@ export const getAllCourses = async (req, res) => {
     }
 }
 //     getCourseById,
+export const getCourseById = async (req, res) => {
+    try {
+        //validate the role must be admin to access this route
+        const tokenCredential = req.user;
+        if (tokenCredential.role !== "admin") {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized",
+            });
+        }
+        const { id } = req.params;
+        const existCourse = await prisma.course.findUnique({
+            where: {
+                id,
+            }
+        })
+        if (!existCourse) {
+            return errorResponse(res, "data tidak ditemukan", null, 404);
+        }
+        const course = await prisma.course.findUnique({
+            where: {
+                id,
+            },
+            include : {
+                lecture : {
+                    major : {
+                        include : {
+                            faculty : true
+                        }
+                    }
+                }
+            }
+        });
+        return successResponse(res, "berhasil mendapatkan data", course);
+    } catch (error) {
+        return errorResponse(res, "terjadi kesalahan", error.message, 500);
+    }
+}
 //     createCourse,
 //     updateCourse,
 //     deleteCourse,
