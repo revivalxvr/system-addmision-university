30d8ea71576243d44b2ae0c39cabde70a2a4e352
revivalxvr <revivaliskandar@gmail.com>
2026-05-25 09:15:50 +0800

    pembuatan 'Tf group by id'

--- src/controllers/TfGroup.controller.js
+++ src/controllers/TfGroup.controller.js
@@ -17,8 +17,36 @@ export const getAllTfGroups = async (req, res) => {
         message: "Unauthorized",
          });
     }
-      const tfGroups = await prisma.tfGroup.findMany();
+      const tfGroups = await prisma.tfGroup.findMany({
+        orderBy: {
+          createdAt: "desc",
+        }
+      });
       return successResponse(res, "berhasil mendapatkan data", tfGroups);
+    } catch (error) {
+      return errorResponse(res, "gagal mendapatkan data", null, 500);
+    }
+  };
+
+  export const getTfGroupById = async (req, res) => {
+    try {
+      const tokenCredential = req.user;
+      if (tokenCredential.role !== "admin") {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+      const { id } = req.params;
+      const tfGroup = await prisma.tfGroup.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!tfGroup) {
+        return errorResponse(res, "data tidak ditemukan", null, 404);
+      }
+      return successResponse(res, "berhasil mendapatkan data", tfGroup);
     } catch (error) {
       return errorResponse(res, "gagal mendapatkan data", null, 500);
     }
