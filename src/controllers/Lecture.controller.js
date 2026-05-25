2e24d6dcd6b36b59a61b15675fa139db11319dbb
revivalxvr <revivaliskandar@gmail.com>
2026-05-25 14:38:55 +0800

    function get dosen by id

--- src/controllers/Lecture.controller.js
+++ src/controllers/Lecture.controller.js
@@ -31,3 +31,40 @@ export const getAllLectures = async (req, res) => {
     return errorResponse(res, "terjadi kesalahan", null, 500);
   }
 };
+
+export const getLectureById = async (req, res) => {
+  try {
+    //validate the role must be admin to access this route
+    const tokenCredential = req.user;
+    if (tokenCredential.role !== "admin") {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const { id } = req.params;
+    const existLecture = await prisma.lecture.findUnique({
+      where: {
+        id,
+      },
+    })
+    if (!existLecture) {
+      return errorResponse(res, "data tidak ditemukan", null, 404);
+    }
+    const lecture = await prisma.lecture.findUnique({
+      where: {
+        id,
+      },
+      include : {
+        major : {
+            include : {
+                faculty : true
+            }
+        }
+    }
+    });
+    return successResponse(res, "berhasil mendapatkan data", lecture);
+  } catch (error) {
+    return errorResponse(res, "terjadi kesalahan", null, 500);
+  }
+};
