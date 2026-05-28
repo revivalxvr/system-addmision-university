aefb7d407860b433589bdacddc4509ae2af03f91
revivalxvr <revivaliskandar@gmail.com>
2026-05-28 10:20:25 +0800

    perubahan router dan buat function get all timeline di controller

--- src/controllers/TimeLine.controller.js
+++ src/controllers/TimeLine.controller.js
@@ -1,3 +1,27 @@
 import prisma from "../config/Prisma";
 import { successResponse, errorResponse } from "../utils/response";
 
+//   getAllTimeLines,
+//     createTimeLine,
+//     updateTimeLine,
+//     deleteTimeLine,
+
+export const getAllTimeLines = async (req, res) => {
+  try {
+    const tokenCredential = req.user;
+    if (tokenCredential.role !== "admin") {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    const timeLines = await prisma.timeLine.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return successResponse(res, "berhasil mendapatkan data", timeLines);
+  } catch (error) {
+    return errorResponse(res, "gagal mendapatkan data", error.message, 500);
+  }
+};
--- src/routes/TimeLine.js
+++ src/routes/TimeLine.js
@@ -2,7 +2,7 @@ import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import {
     getAllTimeLines,
-    getTimeLineById,
+
     createTimeLine,
     updateTimeLine,
     deleteTimeLine,
@@ -12,7 +12,7 @@ const router = express.Router();
 router.use(verifyToken);
 
 router.get("/", getAllTimeLines);
-router.get("/:id", getTimeLineById);
+
 router.post("/", createTimeLine);
 router.put("/:id", updateTimeLine);
 router.delete("/:id", deleteTimeLine);
