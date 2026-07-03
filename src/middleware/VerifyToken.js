ce1bed8ea652addec83e108a7547f598e9f95dd4
revivalxvr <revivaliskandar@gmail.com>
2026-07-03 18:45:35 +0800

    kembalikan kode middleware

--- src/middleware/VerifyToken.js
+++ src/middleware/VerifyToken.js
@@ -1,48 +1,19 @@
-import prisma from '../config/Prisma.js';
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken"
+import { successResponse, errorResponse } from "../utils/response.js";
 
-const JWT_SECRET = process.env.JWT_SECRET || "secret";
+const JWT_SECRET = process.env.JWT_SECRET || "secret"
 
+export const verifyToken = (req,res,next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) return errorResponse(res,"Token required")
 
-export const verifyToken = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
-   
-    // Ambil nilai token terlebih dahulu dari header
-    const token = authHeader && authHeader.split(" ")[1];
-
-    // Baru lakukan pengecekan apakah token tersebut ada atau kosong
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized: Token tidak ditemukan" });
-    }
+    const token = authHeader.split(" ")[1];
 
     try {
-        // Verifikasi token JWT
-        const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-        
-        // Cek apakah data user ini masih ada di database
-        const userAktif = await prisma.userSiakad.findUnique({
-            where: { id: decoded.id } // Pastikan nama field ID sesuai (misal string atau number)
-        });
-
-        // Jika user ternyata sudah dihapus oleh Admin lain dari DB
-        if (!userAktif) {
-            // hapus juga cookie-nya dari sini
-            res.clearCookie("token");
-            res.clearCookie("email");
-            res.clearCookie("userId");
-
-            return res.status(401).json({ 
-                message: "Unauthorized: Akun Anda telah dihapus oleh Admin. Sesi dikeluarkan." 
-            });
-        }
-
-        // Simpan data user asli dari database ke properti req.user (lebih aman daripada data decode JWT saja)
-        req.user = userAktif;
-        
-        // Lolos, lanjut ke controller utama
+        const decoded = jwt.verify(token, JWT_SECRET)
+        req.user = decoded
         next();
     } catch (error) {
-        // Jika token palsu, sudah kedaluwarsa, atau terjadi error DB
-        return res.status(401).json({ message: "Unauthorized: Token tidak valid atau sesi berakhir" });
+        return errorResponse(res,"Token invalid")
     }
-};
+}
